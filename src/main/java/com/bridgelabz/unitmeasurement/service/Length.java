246734fdb3e070c72ceffd3b0d42cb21060f55f5
Amrut <amrutpanda7789@gmail.com>
2020-07-13 00:36:44 +0530

    ADDED: Functionality to compare the units and values and return results accordingly

--- src/main/java/com/bridgelabz/unitmeasurement/service/Length.java
+++ src/main/java/com/bridgelabz/unitmeasurement/service/Length.java
@@ -4,13 +4,33 @@ import com.bridgelabz.unitmeasurement.utility.Unit;
 
 public class Length {
 
+    private static final double FEET_TO_INCH = 12;
     private final Unit unit;
     private final double value;
 
-
+    /**
+     *
+     * @param unit
+     * @param value
+     */
     public Length(Unit unit, double value) {
         this.unit = unit;
         this.value = value;
+    }
+
+    /**
+     *
+     * @param that
+     * @return
+     */
+    public boolean compare(Length that) {
+        if (this.unit.equals(that.unit))
+            return Double.compare(this.value, that.value) == 0;
+        if(this.unit.equals(Unit.FEET) && that.unit.equals(Unit.INCH))
+            return Double.compare(Math.round(this.value*FEET_TO_INCH), that.value) == 0;
+        if(this.unit.equals(Unit.INCH) && that.unit.equals(Unit.FEET))
+            return Double.compare(this.value, Math.round(that.value*FEET_TO_INCH)) == 0;
+        return false;
     }
 
     @Override
