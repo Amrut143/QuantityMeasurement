ff03d63ce941d638d6a3eecb9793ae8d318c725d
Amrut <amrutpanda7789@gmail.com>
2020-07-12 23:50:36 +0530

    ADDED: Test for different feet value should return not equal

--- src/test/java/com/bridgelabz/unitmeasurementtest/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/unitmeasurementtest/QuantityMeasurementTest.java
@@ -13,4 +13,11 @@ public class QuantityMeasurementTest {
         Length feet2 = new Length(Unit.FEET, 0.0);
         Assert.assertEquals(feet1, feet2);
     }
+
+    @Test
+    public void givenDifferentFeetValue_ShouldReturnNotEquals() {
+        Length feet1 = new Length(Unit.FEET, 0.0);
+        Length feet2 = new Length(Unit.FEET, 1.1);
+        Assert.assertNotEquals(feet1, feet2);
+    }
 }
